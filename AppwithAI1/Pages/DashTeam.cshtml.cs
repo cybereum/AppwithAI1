dd57fff6e1e60ce805b313bc919713201a14cd72
cybereum <54966292+cybereum@users.noreply.github.com>
2023-07-27 18:45:00 -0500

    Made use of fourth Quadrant in the Master Dashboard for Team Managemetn and Adding Project Participants. Seperated the Add particiapant forms for internal (withing company, uses Web 2), and Adding external project participants (Web3) Also several corrections and moving of dshboard items to more appropriate places. Comminication has been moved to top menu.

---
+++ AppwithAI1/Pages/DashTeam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppwithAI1.Pages
+{
+    public class DashTeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
